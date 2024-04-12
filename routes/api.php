ce1a0ff177cd14c7b83d4595c7f55e9d6b0f3eb4
olml89 <oleguer.ml89@gmail.com>
2024-04-12 16:54:40 +0200

    Implement a health api endpoint matching the functionality of the web /up Laravel 11 endpoint

--- routes/api.php
+++ routes/api.php
@@ -1,17 +1,10 @@
 <?php declare(strict_types=1);
 
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Route;
-use olml89\XenforoBotsBackend\Reply\Infrastructure\Http\ReceivePost\ReceivePostController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
-Route::post('/posts', ReceivePostController::class);
+Route::get('/', function (): Response {
+    return response([
+        'time' => round((microtime(true) - LARAVEL_START) * 1000),
+    ]);
+});
